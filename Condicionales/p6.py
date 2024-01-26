d0be35581343e1f5748b32e87f3951f535845276
AngeloLeonT <157255387+AngeloLeonT@users.noreply.github.com>
2024-01-26 06:36:52 +0000

    practica

--- Condicionales/p6.py
+++ Condicionales/p6.py
@@ -2,5 +2,13 @@
 # Establecer el precio de la entrada
 # Obtener el resultado dependiendo la edad del cliente
 
-edad = int(input("Ingrese la edad del cliente: "))
+edad = int(input('Ingrese la edad del cliente: '))
 
+if edad < 4 and edad >=0:
+    print(f'El usuario no paga entrada')
+elif edad >= 4 and edad <= 18 and edad >=0:
+    print(f'El usuario debe pagar $5 por entrada.')
+elif edad > 18 and edad >=0:
+    print(f'El usuario debe pagar $10 por entrada.')
+else:
+    print(f'Edad inválida.')

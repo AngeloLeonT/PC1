665e840aaaef0b2804326834b06031f413dfa4c8
AngeloLeonT <157255387+AngeloLeonT@users.noreply.github.com>
2024-01-26 06:39:21 +0000

    practica

--- Condicionales/p6.py
+++ Condicionales/p6.py
@@ -4,11 +4,11 @@
 
 edad = int(input('Ingrese la edad del cliente: '))
 
-if edad < 4 and edad >=0:
+if edad < 4 and edad >0:
     print(f'El usuario no paga entrada')
-elif edad >= 4 and edad <= 18 and edad >=0:
+elif edad >= 4 and edad <= 18 and edad >0:
     print(f'El usuario debe pagar $5 por entrada.')
-elif edad > 18 and edad >=0:
+elif edad > 18 and edad >0:
     print(f'El usuario debe pagar $10 por entrada.')
 else:
     print(f'Edad inválida.')

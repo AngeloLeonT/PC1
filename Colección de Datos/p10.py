3149b7ef252fd0d171cf2d8f22f87cf953fa9a14
AngeloLeonT <157255387+AngeloLeonT@users.noreply.github.com>
2024-01-26 14:48:23 +0000

    prac1

---
+++ Colección de Datos/p10.py
@@ -0,0 +1,11 @@
+# Establecer la lista determinada a usar
+# Esliminación de elementos
+# Obtención del resultado
+
+lista = ['Sony','Apple','Samsung','Xiaomi','Oppo','Huawei']
+
+lista.remove(lista[5])
+lista.remove(lista[4])
+lista.remove(lista[0])
+
+print(lista)
---
+++ Colección de Datos/p11.py
@@ -0,0 +1,9 @@
+# Establecer una lista con elementos repetidos
+# Crear un conjunto 
+# Obtener el resulatdo
+
+lista = ['María', 'Alberto', 'Canada', 'María', 1, 2, 1, 1, 2, 4, 'Canadá']
+
+lista = list(set(lista))
+
+print(lista)
